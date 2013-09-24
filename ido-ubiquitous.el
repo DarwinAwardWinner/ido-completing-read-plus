9835be4cd39439c240fb2d36f1574e783b93fd18
Ryan C. Thompson <rct@thompsonclan.org>
2013-09-24 12:30:56 -0700

    Version 2.4

--- ido-ubiquitous.el
+++ ido-ubiquitous.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ryan C. Thompson
 ;; URL: https://github.com/DarwinAwardWinner/ido-ubiquitous
-;; Version: 2.3
+;; Version: 2.4
 ;; Created: 2011-09-01
 ;; Keywords: convenience
 ;; EmacsWiki: InteractivelyDoThings
@@ -40,7 +40,7 @@
 
 ;;; Code:
 
-(defconst ido-ubiquitous-version "2.3"
+(defconst ido-ubiquitous-version "2.4"
   "Currently running version of ido-ubiquitous.")
 
 (eval-when-compile
