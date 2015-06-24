cb39bc368d3136d356401562a0924d8aa8a5dbf9
Ryan C. Thompson <rct@thompsonclan.org>
2015-06-23 17:48:48 -0700

    Improve setversion.sh

--- setversion.sh
+++ setversion.sh
@@ -4,6 +4,6 @@ TARGET_VERSION="$1"
 if [ -n "$TARGET_VERSION" ]; then
     echo "Updating version to $TARGET_VERSION";
     perl -i'orig_*' -lape "s/Version: [0-9.]+/Version: $TARGET_VERSION/g;" \
-        -e "s/(defconst.*-version\s+)\"[0-9.]+\"/\${1}\"$TARGET_VERSION\"/g;" \
+        -e "s/((?:defconst|defvar|setq).*-version\s+)\"[0-9.]+\"/\${1}\"$TARGET_VERSION\"/g;" \
         *.el
 fi
