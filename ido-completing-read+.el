7354d985d4f529877bd8dcb782940e3e87cf36b2
Ryan C. Thompson <rct@thompsonclan.org>
2016-03-19 23:38:15 -0700

    Version 3.13

--- ido-completing-read+.el
+++ ido-completing-read+.el
@@ -5,7 +5,7 @@
 ;; Filename: ido-completing-read+.el
 ;; Author: Ryan Thompson
 ;; Created: Sat Apr  4 13:41:20 2015 (-0700)
-;; Version: 3.12
+;; Version: 3.13
 ;; Package-Requires: ((emacs "24.1") (cl-lib "0.5"))
 ;; URL: https://github.com/DarwinAwardWinner/ido-ubiquitous
 ;; Keywords: ido, completion, convenience
@@ -41,7 +41,7 @@
 ;;
 ;;; Code:
 
-(defconst ido-completing-read+-version "3.12"
+(defconst ido-completing-read+-version "3.13"
   "Currently running version of ido-ubiquitous.
 
 Note that when you update ido-completing-read+, this variable may
