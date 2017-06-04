86614d22a98b4f7cca3d5528138f9cb78c382b06
Ryan C. Thompson <rct@thompsonclan.org>
2017-06-04 11:47:28 -0400

    Update file headers

--- ido-completing-read+.el
+++ ido-completing-read+.el
@@ -1,6 +1,6 @@
 ;;; ido-completing-read+.el --- A completing-read-function using ido  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2015 Ryan C. Thompson
+;; Copyright (C) 2011-2017 Ryan C. Thompson
 
 ;; Filename: ido-completing-read+.el
 ;; Author: Ryan Thompson
@@ -16,11 +16,47 @@
 ;;
 ;;; Commentary:
 
+;; If you use the excellent `ido-mode' for efficient completion of
+;; file names and buffers, you might wonder if you can get ido-style
+;; completion everywhere else too. Well, that's what this package
+;; does! ido-ubiquitous is here to enable ido-style completion for
+;; (almost) every function that uses the standard completion function
+;; `completing-read'.
+
 ;; This package implements the `ido-completing-read+' function, which
 ;; is a wrapper for `ido-completing-read'. Importantly, it detects
 ;; edge cases that ordinary ido cannot handle and either adjusts them
 ;; so ido *can* handle them, or else simply falls back to Emacs'
-;; standard completion instead.
+;; standard completion instead. Hence, you can safely set
+;; `completing-read-function' to `ido-completing-read+' without
+;; worrying about breaking completion features that are incompatible
+;; with ido.
+
+;; To use this package, call `ido-ubiquitous-mode' to enable the mode,
+;; or use `M-x customize-variable ido-ubiquitous-mode' it to enable it
+;; permanently. Once the mode is enabled, most functions that use
+;; `completing-read' will now have ido completion. If you decide in
+;; the middle of a command that you would rather not use ido, just use
+;; C-f or C-b at the end/beginning of the input to fall back to
+;; non-ido completion (this is the same shortcut as when using ido for
+;; buffers or files).
+
+;; Note that `completing-read-default' is a very general function with
+;; many complex behaviors that ido cannot emulate. This package
+;; attempts to detect some of these cases and avoid using ido when it
+;; sees them. So some functions will not have ido completion even when
+;; this mode is enabled. Some other functions have ido disabled in
+;; them because their packages already provide support for ido via
+;; other means (for example, magit). See `M-x describe-variable
+;; ido-cr+-function-blacklist' for more information.
+
+;; ido-completing-read+ version 4.0 is a major update. The formerly
+;; separate package ido-ubiquitous has been subsumed into
+;; ido-completing-read+, so ido-ubiquitous 4.0 is just a wrapper that
+;; loads ido-completing-read+ and displays a warning about being
+;; obsolete. If you have previously customized ido-ubiquitous, be sure
+;; to check out `M-x customize-group ido-completing-read-plus' after
+;; updating to 4.0 and make sure the new settings are to your liking.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
--- ido-ubiquitous.el
+++ ido-ubiquitous.el
@@ -1,6 +1,6 @@
 ;;; ido-ubiquitous.el --- Use ido (nearly) everywhere. -*- lexical-binding: t -*-
 
-;; Copyright (C) 2011-2015 Ryan C. Thompson
+;; Copyright (C) 2011-2017 Ryan C. Thompson
 
 ;; Author: Ryan C. Thompson
 ;; URL: https://github.com/DarwinAwardWinner/ido-ubiquitous
@@ -17,38 +17,8 @@
 ;;
 ;;; Commentary:
 
-;; If you use the excellent `ido-mode' for efficient completion of
-;; file names and buffers, you might wonder if you can get ido-style
-;; completion everywhere else too. Well, that's what this package
-;; does! ido-ubiquitous is here to enable ido-style completion for
-;; (almost) every function that uses the standard completion function
-;; `completing-read'.
-
-;; To use this package, call `ido-ubiquitous-mode' to enable the mode,
-;; or use `M-x customize-variable ido-ubiquitous-mode' it to enable it
-;; permanently. Once the mode is enabled, most functions that use
-;; `completing-read' will now have ido completion. If you decide in
-;; the middle of a command that you would rather not use ido, just C-f
-;; or C-b at the end/beginning of the input to fall back to non-ido
-;; completion (this is the same shortcut as when using ido for buffers
-;; or files).
-
-;; Note that `completing-read' has some quirks and complex behavior
-;; that ido cannot emulate. Ido-ubiquitous attempts to detect some of
-;; these quirks and avoid using ido when it sees them. So some
-;; functions will not have ido completion even when this mode is
-;; enabled. Some other functions have ido disabled in them because
-;; their packages already provide support for ido via other means (for
-;; example, magit). See `M-x customize-group ido-ubiquitous' and read
-;; about the override variables for more information.
-
-;; ido-ubiquitous version 3.0 is a major update, including a split
-;; into two packages, and some of the configuration options have
-;; changed in non-backwards-compatible ways. If you have customized
-;; ido-ubiquitous, be sure to check out `M-x customize-group
-;; ido-ubiquitous' and `M-x customize-group ido-completing-read+'
-;; after updating to 3.0 and make sure the new settings are to your
-;; liking.
+;; Previously a separate package, ido-ubiquitous has now been subsumed
+;; into ido-completing-read+. You should update your config to install that instead.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
