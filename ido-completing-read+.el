1a1f695eb8e7d4ae2035e506ea3ff5bd4e2d0533
Ryan C. Thompson <rct@thompsonclan.org>
2017-08-07 14:45:22 -0700

    Version 4.3

--- ido-completing-read+.el
+++ ido-completing-read+.el
@@ -5,7 +5,7 @@
 ;; Filename: ido-completing-read+.el
 ;; Author: Ryan Thompson
 ;; Created: Sat Apr  4 13:41:20 2015 (-0700)
-;; Version: 4.2
+;; Version: 4.3
 ;; Package-Requires: ((emacs "24.4") (cl-lib "0.5") (s "0.1"))
 ;; URL: https://github.com/DarwinAwardWinner/ido-completing-read-plus
 ;; Keywords: ido, completion, convenience
@@ -77,7 +77,7 @@
 ;;
 ;;; Code:
 
-(defconst ido-completing-read+-version "4.2"
+(defconst ido-completing-read+-version "4.3"
   "Currently running version of ido-completing-read+.
 
 Note that when you update ido-completing-read+, this variable may
