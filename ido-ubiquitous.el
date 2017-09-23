e2ea358725f03ae623ae03ed90715efb92a61030
Ryan C. Thompson <rct@thompsonclan.org>
2017-09-23 08:42:29 -0700

    Remove required Emacs version from ido-ubiquitous
    Fixes #139.

--- ido-ubiquitous.el
+++ ido-ubiquitous.el
@@ -8,7 +8,7 @@
 ;; Created: 2011-09-01
 ;; Keywords: convenience, completion, ido
 ;; EmacsWiki: InteractivelyDoThings
-;; Package-Requires: ((emacs "24.1") (ido-completing-read+ "4.6") (cl-lib "0.5"))
+;; Package-Requires: ((ido-completing-read+ "4.6") (cl-lib "0.5"))
 ;; Filename: ido-ubiquitous.el
 
 ;; This file is NOT part of GNU Emacs.
