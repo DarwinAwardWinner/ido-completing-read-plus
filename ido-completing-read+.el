DarwinAwardWinner/ido-completing-read-plus

commit d3493021f4d825c7a7cf57a9b34bd262e4ff9927

Add a better workaround for #146

ido-completing-read+.el
@@ -520,6 +520,12 @@ completion for them."
           (if ido-cr+-dynamic-collection
               (memoize (indirect-function 'all-completions))
             'all-completions))
+         ;; Disable flx-ido for dynamic collections in Emacs 26.
+         ;; Temporary workaround for #146.
+         (flx-ido-mode
+          (and flx-ido-mode
+               (or (version< emacs-version "26")
+                   (not ido-cr+-dynamic-collection))))
          ;; If the whitelist is empty, everything is whitelisted
          (whitelisted (not ido-cr+-function-whitelist))
          ;; If non-nil, we need alternate nil DEF handling

commit cb9aa780addb9d9b24a7be777d9fc70807596fa4

Fix void variable error introduced in d349302
Fixes #147.

ido-completing-read+.el
@@ -523,7 +523,7 @@ completion for them."
          ;; Disable flx-ido for dynamic collections in Emacs 26.
          ;; Temporary workaround for #146.
          (flx-ido-mode
-          (and flx-ido-mode
+          (and (bound-and-true-p flx-ido-mode)
                (or (version< emacs-version "26")
                    (not ido-cr+-dynamic-collection))))
          ;; If the whitelist is empty, everything is whitelisted