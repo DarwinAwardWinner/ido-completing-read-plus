1d053a424cc999b31f07fc03c86895dff61d8d47
Ryan C. Thompson <rct@thompsonclan.org>
2020-03-09 07:22:03 -0700

    Enable tests in emacs-snapshot
    Undercover is currently disabled for emacs-snapshot because it needs
    to be updated.

--- 001-setup-undercover.el
+++ 001-setup-undercover.el
@@ -1,10 +1,11 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t; no-byte-compile: t -*-
 
 
-(when (featurep 'ido-completing-read+)
-  (error "Undercover loaded too late"))
-(message "Setting up undercover")
-(require 'undercover)
-(defadvice undercover--edebug-files (before echo-instrumented-files activate)
-  (message "Undercover instrumenting the following files: %S" files))
-(undercover "ido-completing-read+.el")
+(when (version< emacs-version "27")
+  (when (featurep 'ido-completing-read+)
+    (error "Undercover loaded too late"))
+  (message "Setting up undercover")
+  (require 'undercover)
+  (defadvice undercover--edebug-files (before echo-instrumented-files activate)
+    (message "Undercover instrumenting the following files: %S" files))
+  (undercover "ido-completing-read+.el"))
